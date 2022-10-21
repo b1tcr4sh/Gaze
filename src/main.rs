ea1d951261cf34b7b955b95472e5b4fa7e84d2ef
Kyra <ethan.ford06@gmail.com>
2022-10-21 09:54:37 -0500

    Fixed packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+
+    if args.len() <= 1 {
+        eprintln!("No args passed... ?");
+        return;
+    }
+
+
+    let command: &String = &args[1];
+
+    if command == &String::from("help") {
+        println!("Help message ahhhh");
+    }
 }
