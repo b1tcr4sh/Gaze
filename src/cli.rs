1e193d12db25b0a83c1b5cd1a838476e1734e8c8
Ethan Ford <ethan.ford06@gmail.com>
2022-10-31 21:03:10 -0500

    Implemented clap

---
+++ src/cli.rs
@@ -0,0 +1,52 @@
+use clap:: {
+    Args,
+    Parser,
+    Subcommand
+};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct WayfarerCli {
+    #[clap(subcommand)]
+    pub operation: Operation
+
+}
+#[derive(Debug, Subcommand)]
+pub enum Operation {
+    Add(AddCommand),
+    Sync,
+    Generate,
+    Profile(ProfileCommand),
+    List
+}
+
+#[derive(Debug, Args)]
+pub struct ProfileCommand {
+    #[clap(subcommand)]
+    pub subcommand: ProfileSubcommand
+}
+
+#[derive(Debug, Subcommand)] 
+pub enum ProfileSubcommand {
+    Create(CreateSubcommand),
+    Delete(DeleteSubcommand),
+    List
+}
+
+#[derive(Debug, Args)] 
+pub struct AddCommand {
+    pub id: String,
+}
+
+#[derive(Debug, Args)]
+pub struct CreateSubcommand {
+    name: String,
+    minecraft_version: String,
+    mod_loader: String,
+    is_server_side: bool
+}
+
+#[derive(Debug, Args)]
+pub struct DeleteSubcommand {
+    name: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
 mod api_client;
-use std::env;
+mod cli;
+
+use clap::Parser;
+use cli::WayfarerCli;
+
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
 
+    let args = WayfarerCli::parse();
 
-    if args.len() <= 1 {
-        eprintln!("No args passed... ?");
-        return;
-    }
-
-    let command: &String = &args[1];
-
-    if command == &String::from("help") {
-        println!("Help message ahhhh");
-        return;
-    }
-    
-
-    let mod_res =  api_client::get_mod(api_client::Service::Modrinth, &args[1]).await;
-
-    if !mod_res.is_ok() {
-        println!("Couldn't complete request.. ?");
-        return;
-    }
-
-    print_search(&mod_res.unwrap().hits);
-}
-
-fn print_search(hits: &[api_client::Hit]) {
-    for hit in hits {
-        println!("==");
-        print!("{}\n\n{}\n", &hit.title, &hit.description);
-        print!("Uploaded by: {}\n", &hit.author);
-    }
 }
