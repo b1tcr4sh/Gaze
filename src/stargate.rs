8fc3a31ac2a4e8048bdca2e5dc88e2bb5eb3a9ce
Kyra <ethan.ford06@gmail.com>
2022-12-07 15:44:15 -0600

    ahhh

--- src/stargate.rs
+++ src/stargate.rs
@@ -1,13 +1,17 @@
 use zbus::dbus_proxy;
+use zbus::Connection;
+use zvariant::ObjectPath;
 
-use self::data_models::*;
+use self::profile_model::*;
+use self::profile_mess_model::*;
 
-mod data_models;
+mod profile_model;
+mod profile_mess_model;
 
 #[dbus_proxy(
     interface = "org.mercurius.profile",
     default_service = "org.mercurius.profile",
-    default_path = "/org/mercurius/profile/uwu"
+    default_path = "/org/mercurius/profile/"
 )]
 trait Profile {
     fn GetProfileInfo(&self) -> zbus::Result<ProfileInfo>;
@@ -18,3 +22,20 @@ trait Profile {
     fn Verify(&self) -> zbus::Result<ValidityReport>;
 }
 
+pub async fn Connect() -> Connection {
+    let connection = Connection::session().await;
+
+    match connection {
+        Ok(connection ) => {
+            return connection;
+        },
+        Err(why) => {
+            panic!("Failed to connect to session bus!: {:?}", why);
+        }
+    }
+}
+pub async fn GetProfiles(&connection: Connection ) -> Vec<ProfileProxy> {
+    let Ok(messenger) = ProfileMessengerProxy::new(connection).await;
+
+    profiles = messenger.ListProfiles();
+}
